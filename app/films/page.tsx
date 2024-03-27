812faa4d13382afe58a2d3d7fa132794ccfb5d00
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2024-03-27 04:00:40 +0100

    feat: Implement random movie banner on movie page
    - Created BannerPage component to display a random movie banner fetched from popular movies.
    - Utilized useGetPopularMovie hook to fetch popular movies data.
    - Implemented logic to randomly select a movie from the fetched data and display its details.
    - Added loading state handling for the banner section.

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client";
 import {
   ContainerScroll,
   HeaderContainer,
@@ -10,15 +11,19 @@ import Footer from "@/components/footer/footer";
 import { Button, ButtonLink } from "@/components/button/button";
 import { FaPlay } from "react-icons/fa";
 import { MdStarRate } from "react-icons/md";
-import { TypeMovieOverview } from "@/types/movie";
+import { TypeMovieDetails, TypeMovieOverview } from "@/types/movie";
 import { fakeDataPopularMovie } from "@/data/fakeData.PopularMovie";
 import { CardMovie } from "@/components/card/card";
 import { TitleSection } from "@/components/titleSection/titleSection";
+import { useGetPopularMovie } from "@/hooks/useMovie";
+import { useEffect, useState, useMemo } from "react";
 interface PropsMovieComponent {
   movie: TypeMovieOverview;
 }
 
-const BannerPage: React.FC<PropsMovieComponent> = ({ movie }) => {
+const BannerPage = () => {
+  const { data, isLoading, isError } = useGetPopularMovie(1);
+  const [randomMovie, setRandomMovie] = useState<TypeMovieDetails>();
   const ratingCountIcons = (rating: number): React.ReactNode => {
     let containerRating: React.ReactNode[] = [];
     for (let index = 0; index < rating; index++) {
@@ -26,31 +31,55 @@ const BannerPage: React.FC<PropsMovieComponent> = ({ movie }) => {
     }
     return containerRating;
   };
-  return (
+  useEffect(() => {
+    if (!isLoading && data !== null) {
+      const randomIndex: number = Math.floor(Math.random() * data.length - 1);
+      setRandomMovie(data[randomIndex]);
+    }
+  }, [isLoading, data]);
+  const displayContainer = isLoading ? (
+    <section
+      className="banner-loading section__page loading__container"
+      id="banner__movie"
+    >
+      <div className="skeleton-loading"></div>
+    </section>
+  ) : (
     <section
       className="section__page"
       id="banner__movie"
       style={{
-        background: `url("${movie.poster}") center/cover fixed`,
+        background: `url("https://image.tmdb.org/t/p/original${
+          randomMovie && randomMovie.poster_path
+        }") center/cover fixed`,
       }}
     >
-      <h1>{movie.title}</h1>
+      <h1>{randomMovie && randomMovie.title}</h1>
       <div className="details__movie">
-        <div className="rating">{ratingCountIcons(movie.ratingCount)}</div>{" "}
-        <p className="rate_prct">{movie.ratingCount.toFixed(1)}</p>{" "}
-        <p className="years">{movie.releaseDate}</p>
+        <div className="rating">
+          {randomMovie &&
+            ratingCountIcons(
+              Math.round(Math.max(0, Math.min(5, randomMovie.vote_average)))
+            )}
+        </div>
+        <p className="rate_prct">
+          {randomMovie && randomMovie.vote_count.toFixed(1)}
+        </p>
+        <p className="years">{randomMovie && randomMovie.release_date}</p>
       </div>
-      <p className="overview">{movie.overview}</p>
+      <p className="overview">{randomMovie && randomMovie.overview}</p>
+
       <div className="controll-action-button">
         <Button variant="primary">
           Voir le trailer <FaPlay />
         </Button>
-        <ButtonLink variant="secondary" href="/movies/12">
+        <ButtonLink variant="secondary" href={`/movies/${randomMovie?.id}`}>
           Plus d'infos
         </ButtonLink>
       </div>
     </section>
   );
+  return displayContainer;
 };
 
 const ContainerAllMovies = () => {
@@ -146,7 +175,7 @@ export default function FilmPage() {
     <main className="container__page" id="filmPage">
       <Suspense fallback={<LoaderPage />}>
         <HeaderContainer />
-        <BannerPage movie={fakeDataPopularMovie[0]} />
+        <BannerPage />
         <ContainerActionMoivies />
         <ContainerDrameMoivies />
         <ContainerMusicMoivies />
